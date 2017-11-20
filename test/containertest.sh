2e085e194aeccc49a558beed73132625d6d060c0
Randy Fay <randy@randyfay.com>
2017-11-20 11:46:29 -0700

    Fix containertest.sh to use php5

--- test/containertest.sh
+++ test/containertest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -o errexit
 set -o pipefail
@@ -6,25 +6,25 @@ set -o nounset
 
 HOST_PORT="1081"
 CONTAINER_PORT="80"
+CONTAINER_NAME=web-local-test
 
-docker stop web-local-test || true
-docker rm web-local-test || true
+docker rm -f web-local-test || true
 
 echo "checking tool versions"
-docker run -it `awk '{print $1}' .docker_image` php --version | grep "PHP 7"
+docker run -it `awk '{print $1}' .docker_image` php --version | grep "PHP 5"
 docker run -it `awk '{print $1}' .docker_image` drush --version
 docker run -it `awk '{print $1}' .docker_image` wp --version
 
 echo "starting container for tests"
 docker run -p $HOST_PORT:$CONTAINER_PORT -e "DOCROOT=docroot" -d --name web-local-test -d `awk '{print $1}' .docker_image`
-CONTAINER_NAME=web-local-test ./test/containercheck.sh
+./test/containercheck.sh
 
 echo "testing php and email"
 curl --fail localhost:$HOST_PORT/test/phptest.php
 curl -s localhost:$HOST_PORT/test/test-email.php | grep "Test email sent"
-docker stop web-local-test && docker rm web-local-test
+docker rm -f $CONTAINER_NAME
 
 echo "testing use of custom nginx config"
-docker run -p $HOST_PORT:$CONTAINER_PORT -e "DOCROOT=potato" -v `pwd`/test/test-custom.conf:/var/www/html/.ddev/nginx-site.conf -d --name web-local-test -d `awk '{print $1}' .docker_image`
-docker exec -it web-local-test cat /etc/nginx/sites-enabled/nginx-site.conf | grep "docroot is /var/www/html/potato in custom conf"
-docker stop web-local-test && docker rm web-local-test
+docker run -p $HOST_PORT:$CONTAINER_PORT -e "DOCROOT=potato" -v `pwd`/test/test-custom.conf:/var/www/html/.ddev/nginx-site.conf -d --name $CONTAINER_NAME `awk '{print $1}' .docker_image`
+docker exec -it $CONTAINER_NAME cat /etc/nginx/sites-enabled/nginx-site.conf | grep "docroot is /var/www/html/potato in custom conf"
+docker rm -f $CONTAINER_NAME
